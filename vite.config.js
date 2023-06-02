87f4c65e5f241528bfe28678efd001d25a2407c5
stanw0ng <user.stanley@gmail.com>
2023-06-01 23:19:24 -0400

    configure normalize.css

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sassPlugin from 'vite-plugin-sass';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sassPlugin(),
+  ],
   server: {
     port: 3000,
   },
-})
+});
