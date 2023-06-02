f9f8533fd032a0a3d9852c066f5586902093f3a0
stanw0ng <user.stanley@gmail.com>
2023-06-02 01:04:04 -0400

    configure local fonts

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sassPlugin from 'vite-plugin-sass';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sassPlugin(),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+      },
+    },
   },
 });
