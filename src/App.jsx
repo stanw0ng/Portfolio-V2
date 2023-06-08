bd47623febe6e612a4e555baecca7491419f31b6
stanw0ng <user.stanley@gmail.com>
2023-06-08 16:36:03 -0400

    add perlin noise background

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ShaderCanvas from './components/ShaderCanvas'
+import Navigation from './components/Navigation'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>The quick brown fox jumps over the lazy dog</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ShaderCanvas/>
+      <Navigation/>
+      <h1 className='banner'>👷‍♂️ Website Under Construction... 🚧</h1>
     </>
   )
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Navigation() {
+ 
+  return (
+    <nav>
+      <h2>Info</h2>
+      <h2>Work</h2>
+      <h2>Contact</h2>
+    </nav>
+  )
+}
+
+export default Navigation
