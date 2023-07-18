949d796d94a9ef29deb7cb89251ffaeca1b53f3c
stanw0ng <user.stanley@gmail.com>
2023-07-18 15:37:39 -0400

    change vite.config.js for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio-V2/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -10,7 +9,6 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        implementation: require('sass')
       },
     },
   },
